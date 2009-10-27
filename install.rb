e3b31fa2a1879fe8a01f603e10d42b713d66646b
cameron <cameron@snepo.com>
2009-10-27 12:48:03 +1100

    adding some puts to the install.rb

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
 # Install hook code here
+puts "Copying ghost_form.js to public/javascript"
+puts "Ensure that you have jQuery 1.3.2 installed also"
 require 'fileutils'
 FileUtils.cp((File.join(File.dirname(__FILE__), 'javascript', 'ghost_form.js')), "#{Rails.root}/public/javascripts/")
