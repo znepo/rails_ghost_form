26a5ef7f91665b2f2430da415bdaac20a5484fba
cameron <cameron@snepo.com>
2009-10-27 12:50:46 +1100

    spelling error in the install

--- install.rb
+++ install.rb
@@ -2,4 +2,4 @@
 puts "Copying ghost_form.js to public/javascript"
 puts "Ensure that you have jQuery 1.3.2 installed also"
 require 'fileutils'
-FileUtils.cp((File.join(File.dirname(__FILE__), 'javascript', 'ghost_form.js')), "#{Rails.root}/public/javascripts/")
+FileUtils.cp((File.join(File.dirname(__FILE__), 'javascripts', 'ghost_form.js')), "#{Rails.root}/public/javascripts/")
