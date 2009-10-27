9c4b9c04b35a2d7588226e677602c402c925e4af
cameron <cameron@snepo.com>
2009-10-27 12:58:25 +1100

    Adding a variety of fixes

--- install.rb
+++ install.rb
@@ -1,7 +1,9 @@
 # Install hook code here
-
+require File.join(File.dirname(__FILE__), "../../../config/environment")
 require 'fileutils'
-ghost_form = File.join(File.dirname(__FILE__), 'javascripts', 'ghost_form.js')
+ghost_form =  "#{Rails.root}/vendor/plugins/rails_ghost_form/javascripts/ghost_form.js"
 puts "Copying #{ghost_form} to #{Rails.root}/public/javascript"
 puts "Ensure that you have jQuery 1.3.2 installed also"
-FileUtils.cp((), "#{Rails.root}/public/javascripts/")
+
+
+FileUtils.cp(ghost_form, "#{Rails.root}/public/javascripts/ghost_form.js")
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,4 @@
 # Uninstall hook code here
+require File.join(File.dirname(__FILE__), "../../../config/environment")
+require 'fileutils'
+FileUtils.rm("#{Rails.root}/public/javascripts/ghost_form.js")
